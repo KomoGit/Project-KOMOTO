aafc734a95a16a58d8470fb11e7739b48b7ca225
Elnur <entreur@gmail.com>
2023-09-30 12:59:37 +0400

    Buttons contain custom links of jobs.

--- Telegram Bot/main.go
+++ Telegram Bot/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"strconv"
@@ -8,28 +9,40 @@ import (
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
-var numericKeyboard = tgbotapi.NewInlineKeyboardMarkup(
-	tgbotapi.NewInlineKeyboardRow(
-		tgbotapi.NewInlineKeyboardButtonURL("Apply \U00002705", "example.com"),
-		tgbotapi.NewInlineKeyboardButtonData("Save \U0001F4BE", ""),
-	),
-)
+// var numericKeyboard = tgbotapi.NewInlineKeyboardMarkup(
+// 	tgbotapi.NewInlineKeyboardRow(
+// 		tgbotapi.NewInlineKeyboardButtonURL("Apply \U00002705", "example.com"),
+// 		tgbotapi.NewInlineKeyboardButtonData("Save \U0001F4BE", "example.com"),
+// 	),
+// )
 
 var chatId int64 = GetChatId(os.Getenv("TELEGRAM_CHAT_ID"))
 
 type Job struct {
+	title       string
+	description string
+	link        string
 }
 
 func main() {
+	// var jobs []Job
+	// Insert a sample job into the slice
+	sampleJob := Job{
+		title:       "Software Engineer",
+		description: "Develop software applications.",
+		link:        "google.com",
+	}
+
+	// // Append the sample job to the slice
+	// jobs = append(jobs, sampleJob)
+
 	bot, err := tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_APITOKEN"))
 	if err != nil {
 		log.Panic(err)
 	}
 
 	bot.Debug = true
-
 	log.Printf("Authorized on account %s", bot.Self.UserName)
-
 	u := tgbotapi.NewUpdate(0)
 	u.Timeout = 60
 
@@ -41,14 +54,8 @@ func main() {
 		if update.Message != nil {
 			// Construct a new message from the given chat ID and containing
 			// the text that we received.
-			msg := tgbotapi.NewMessage(chatId, update.Message.Text) // update.Message.Chat.ID
-			msg.ReplyMarkup = numericKeyboard
-			// If the message was open, add a copy of our numeric keyboard.
-			// switch update.Message.Text {
-			// case "open":
-			// 	msg.ReplyMarkup = numericKeyboard
-			// }
-
+			msg := tgbotapi.NewMessage(chatId, SendJob(sampleJob))
+			msg.ReplyMarkup = SendKeyboard(sampleJob)
 			// Send the message.
 			if _, err = bot.Send(msg); err != nil {
 				panic(err)
@@ -75,4 +82,16 @@ func GetChatId(envVar string) int64 {
 		return n
 	}
 	return 0
+}
+
+func SendJob(job Job) string {
+	return fmt.Sprintf("%s \n%s", job.title, job.description)
+}
+
+func SendKeyboard(job Job) tgbotapi.InlineKeyboardMarkup {
+	return tgbotapi.NewInlineKeyboardMarkup(
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonURL("Apply \U00002705", job.link),
+			tgbotapi.NewInlineKeyboardButtonData("Save \U0001F4BE", "example.com"),
+		))
 }
