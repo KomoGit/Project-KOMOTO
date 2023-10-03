21c68ca0829298262c48e44f943a22a89e3e88da
Elnur <entreur@gmail.com>
2023-10-03 18:04:39 +0400

    Company data added.

--- Testing Application/Controllers/JobController.cs
+++ Testing Application/Controllers/JobController.cs
@@ -20,15 +20,33 @@ namespace TestingApplication.Controllers
         [HttpGet]
         public IActionResult Index()
         {
-            List<Job>? items = _context.Jobs.Include("JobCategory").ToList();
+            List<Job>? items = _context.Jobs.Include("JobCategory").Include("Employer").ToList();
             return Ok(items);
         }
         [HttpGet]
         [Route("/api/catid")]
         //Not including ("JobCategory") because the channel names are the categories.
-        public IActionResult GetSpecificJobs(int id = 1)
+        public IActionResult GetJobsByCategory(int id = 1)
         {
-            List<Job>? items = _context.Jobs.Where(j => j.CategoryId == id).ToList();
+            List<Job>? items = _context.Jobs
+                .Where(j => j.CategoryId == id)
+                .Include("Employer")
+                .ToList();
+            return Ok(items);
+        }
+        [HttpGet]
+        [Route("/api/comid")]
+        public IActionResult GetJobsByCompany(int id)
+        {
+            if(id == 0)
+            {
+                return BadRequest("Id cannot be 0");
+            }//For debug reasons the employer is also included but remove it after.
+            List<Job>? items = _context.Jobs
+                .Where(j => j.EmployerId == id)
+                .Include("JobCategory")
+                .Include("Employer")
+                .ToList();
             return Ok(items);
         }
     }
---
+++ Testing Application/Model/Company.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TestingApplication.Model
+{
+    public class Company:BaseModel
+    {
+        [Required]
+        [MaxLength(100,ErrorMessage = "Cannot exceed 100")]
+        public string Name { get; set; }
+        [NotMapped]
+        public List<Job>? ActiveJobs { get; set; }
+    }
+}
