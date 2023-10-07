13050cc9d6f24d758fcaee3b4e54bc53f4083872
Elnur <entreur@gmail.com>
2023-10-07 16:13:39 +0400

    Renamed the data

---
+++ Telegram Bot/structs.go
@@ -0,0 +1,28 @@
+package main
+
+type Category struct {
+	Name string `json:"name"`
+	ID   int    `json:"id"`
+}
+
+type Company struct {
+	Name     string `json:"name"`
+	Industry string `json:"industry"`
+}
+
+type Job struct {
+	ID          int      `json:"id"`
+	CompanyID   int      `json:"employerId"`
+	CategoryID  int      `json:"categoryId"`
+	Title       string   `json:"title"`
+	Description string   `json:"description"`
+	Link        string   `json:"link"`
+	ExpDate     string   `json:"expirationDate"`
+	Cat         Category `json:"jobCategory"`
+	Employer    Company  `json:"employer"`
+}
+
+type BotConfig struct {
+	api_link string
+	chat_id  int64
+}
