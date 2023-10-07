09d5401c0df253adec27c412ab071fea0aaa89cd
Elnur <entreur@gmail.com>
2023-10-07 16:15:14 +0400

    Separating parts of project

--- Telegram Bot/parser.go
+++ Telegram Bot/parser.go
@@ -6,6 +6,13 @@ import (
 	"strings"
 )
 
+func CombineData(link string, id int64) BotConfig {
+	return BotConfig{
+		api_link: link,
+		chat_id:  id,
+	}
+}
+
 func SplitData(link string) []string {
 	return strings.Split(link, " ")
 }
@@ -14,7 +21,7 @@ func getEnvBool() bool {
 	return strings.ToLower(os.Getenv("DEBUG_MODE")) == "true"
 }
 
-func GetChatId(str string) int64 {
+func ConvertChatId(str string) int64 {
 	if n, err := strconv.ParseInt(str, 10, 64); err == nil {
 		return n
 	}
