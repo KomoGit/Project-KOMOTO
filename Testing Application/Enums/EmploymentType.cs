587aee461bd0de8c3c5d916edd31f96276ec420a
Elnur <entreur@gmail.com>
2023-10-13 15:57:24 +0400

    Enum data types

---
+++ Testing Application/Enums/EmploymentType.cs
@@ -0,0 +1,10 @@
+﻿namespace TestingApplication.Enums
+{
+    public enum EmploymentType
+    {
+        FULLTIME = 1,
+        PARTTIME = 2,
+        INTERNSHIP = 3,
+        FREELANCE = 4,
+    }
+}
---
+++ Testing Application/Enums/LocationType.cs
@@ -0,0 +1,9 @@
+﻿namespace TestingApplication.Enums
+{
+    public enum LocationType
+    {
+        REMOTE = 1,
+        OFFICE = 2,
+        HYBRID = 3,
+    }
+}
