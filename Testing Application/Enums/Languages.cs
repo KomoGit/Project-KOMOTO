b18b8ddd3cf4ff1c27aed66536375ad959e53e02
Elnur <entreur@gmail.com>
2023-10-14 11:48:40 +0400

    ++

---
+++ Testing Application/Enums/Languages.cs
@@ -0,0 +1,11 @@
+﻿namespace TestingApplication.Enums
+{
+    public enum Languages
+    {
+        AzerbaijaniTurkish = 1,
+        English = 2,
+        Turkish = 3,
+        German = 4,
+        Russian = 5,      
+    }
+}
