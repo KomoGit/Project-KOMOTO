ab810bc134669ff37c8905df055537b7a21f5c17
Elnur <entreur@gmail.com>
2023-10-20 20:04:55 +0400

    File upload mechanism

---
+++ Testing Application/Libraries/Repository/IFileManager.cs
@@ -0,0 +1,9 @@
+﻿namespace TestingApplication.Libraries.Repository
+{
+    public interface IFileManager
+    {
+        string Upload(IFormFile file, string _PATH);
+        void Delete(string fileName);
+        bool FileExists(string fileName, string _PATH);
+    }
+}
