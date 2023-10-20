61a3b494df7bb53204eb88a8aa1c27dfc26b0772
Elnur <entreur@gmail.com>
2023-10-20 20:08:07 +0400

    New get methods

--- Testing Application/Controllers/JobController.cs
+++ Testing Application/Controllers/JobController.cs
@@ -1,13 +1,12 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Newtonsoft.Json;
 using TestingApplication.Authentication;
 using TestingApplication.Data;
 using TestingApplication.Data_Transfer_Objects;
 using TestingApplication.Model;
 
-namespace TestingApplication.Controllers
+namespace TestingApplication.Controllers.V1
 {
     [Route("api/jobs")]
     [ApiController]
@@ -15,7 +14,7 @@ namespace TestingApplication.Controllers
     {
         public readonly ApplicationDbContext _context;
         public readonly IMapper _mapper;
-        public JobController(ApplicationDbContext context,IMapper mapper)
+        public JobController(ApplicationDbContext context, IMapper mapper)
         {
             _mapper = mapper;
             _context = context;
@@ -24,12 +23,11 @@ namespace TestingApplication.Controllers
         [ServiceFilter(typeof(ApiKeyAuthFilter))]
         public IActionResult Index()
         {
-            var items = _context.Jobs
+            List<JobDTO>? items = _context.Jobs
                 .Select(job => new JobDTO
                 {
                     Id = job.Id,
                     CategoryId = job.CategoryId,
-                    EmployerId = job.EmployerId,
 
                     Title = job.Title,
                     Description = job.Description,
@@ -39,21 +37,21 @@ namespace TestingApplication.Controllers
 
                     ExpirationDate = job.ExpirationDate,
                     PublishDate = job.PublishDate,
-                    
+
                     Link = job.Link,
-                   
+
                     NumberOfHires = job.NumberOfHires,
                     NumberOfViews = job.NumberOfViews,
 
                     EmploymentType = job.EmploymentType,
                     LocationType = job.LocationType,
-                    
+
                     MaximumAgeRequirement = job.MaximumAgeRequirement,
                     MinimumAgeRequirement = job.MinimumAgeRequirement,
                     MinimumExperienceInYears = job.MinimumExperienceInYears,
 
                     MenNeedNotApply = job.MenNeedNotApply,
-                    WomenNeedNotApply = job.WomenNeedNotApply,             
+                    WomenNeedNotApply = job.WomenNeedNotApply,
                 })
                 .ToList();
             return Ok(items);
@@ -63,7 +61,7 @@ namespace TestingApplication.Controllers
         //Not including ("JobCategory") because the channel names are the categories.
         public IActionResult GetJobsByCategory(int id = 1)
         {
-            List<Job>? items = _context.Jobs 
+            List<Job>? items = _context.Jobs
                 .Where(j => j.CategoryId == id && j.ExpirationDate >= DateTime.Today)
                 .Include("Employer")
                 .ToList();
@@ -74,25 +72,125 @@ namespace TestingApplication.Controllers
         [Route("/api/companyid")]
         public IActionResult GetJobsByCompany(int id)
         {
-            if(id == 0)
+            if (id == 0)
             {
                 return BadRequest("Id cannot be 0");
             }
-            //For debug reasons the employer is also included but remove it after.
-            List<Job>? items = _context.Jobs
-                .Where(j => j.EmployerId == id && j.ExpirationDate >= DateTime.Today)
-                .Include("JobCategory")
-                .Include("Employer")
+            List<JobDTO>? items = _context.Jobs
+                .Select(job => new JobDTO
+                {
+                    Id = job.Id,
+                    CategoryId = job.CategoryId,
+
+                    Title = job.Title,
+                    Description = job.Description,
+
+                    Employer = job.Employer,
+                    JobCategory = job.JobCategory,
+
+                    ExpirationDate = job.ExpirationDate,
+                    PublishDate = job.PublishDate,
+
+                    Link = job.Link,
+
+                    NumberOfHires = job.NumberOfHires,
+                    NumberOfViews = job.NumberOfViews,
+
+                    EmploymentType = job.EmploymentType,
+                    LocationType = job.LocationType,
+
+                    MaximumAgeRequirement = job.MaximumAgeRequirement,
+                    MinimumAgeRequirement = job.MinimumAgeRequirement,
+                    MinimumExperienceInYears = job.MinimumExperienceInYears,
+
+                    MenNeedNotApply = job.MenNeedNotApply,
+                    WomenNeedNotApply = job.WomenNeedNotApply,
+                })
+                .Where(j => j.Employer!.Id == id && j.ExpirationDate < DateTime.Today)
                 .ToList();
             return Ok(items);
         }
+
+        [HttpGet]
+        [Route("/api/companyandcategory")]
+        public IActionResult GetJobsByCompanyAndCategory(int id, int categoryId)
+        {
+            if (id == 0)
+            {
+                return BadRequest("Id cannot be 0");
+            }
+            List<JobDTO>? items = _context.Jobs
+                .Select(job => new JobDTO
+                {
+                    Id = job.Id,
+                    CategoryId = job.CategoryId,
+
+                    Title = job.Title,
+                    Description = job.Description,
+
+                    Employer = job.Employer,
+                    JobCategory = job.JobCategory,
+
+                    ExpirationDate = job.ExpirationDate,
+                    PublishDate = job.PublishDate,
+
+                    Link = job.Link,
+
+                    NumberOfHires = job.NumberOfHires,
+                    NumberOfViews = job.NumberOfViews,
+
+                    EmploymentType = job.EmploymentType,
+                    LocationType = job.LocationType,
+
+                    MaximumAgeRequirement = job.MaximumAgeRequirement,
+                    MinimumAgeRequirement = job.MinimumAgeRequirement,
+                    MinimumExperienceInYears = job.MinimumExperienceInYears,
+
+                    MenNeedNotApply = job.MenNeedNotApply,
+                    WomenNeedNotApply = job.WomenNeedNotApply,
+                })
+                .Where(j =>
+                j.Employer!.Id == id &&
+                j.CategoryId == categoryId &&
+                j.ExpirationDate < DateTime.Today)
+                .ToList();
+            return Ok(items);
+        }
+
         [HttpGet]
         [Route("/api/archive")]
         public IActionResult GetArchivedJobs()
         {
-            List<Job>? items = _context.Jobs
-                .Include("JobCategory")
-                .Include("Employer")
+            List<JobDTO>? items = _context.Jobs
+                .Select(job => new JobDTO
+                {
+                    Id = job.Id,
+                    CategoryId = job.CategoryId,
+
+                    Title = job.Title,
+                    Description = job.Description,
+
+                    Employer = job.Employer,
+                    JobCategory = job.JobCategory,
+
+                    ExpirationDate = job.ExpirationDate,
+                    PublishDate = job.PublishDate,
+
+                    Link = job.Link,
+
+                    NumberOfHires = job.NumberOfHires,
+                    NumberOfViews = job.NumberOfViews,
+
+                    EmploymentType = job.EmploymentType,
+                    LocationType = job.LocationType,
+
+                    MaximumAgeRequirement = job.MaximumAgeRequirement,
+                    MinimumAgeRequirement = job.MinimumAgeRequirement,
+                    MinimumExperienceInYears = job.MinimumExperienceInYears,
+
+                    MenNeedNotApply = job.MenNeedNotApply,
+                    WomenNeedNotApply = job.WomenNeedNotApply,
+                })
                 .Where(j => j.ExpirationDate < DateTime.Today)
                 .ToList();
             return Ok(items);
