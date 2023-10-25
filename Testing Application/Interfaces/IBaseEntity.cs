337b3a5c94e2bbd66fbdfc75cd568759b80b922d
Elnur <entreur@gmail.com>
2023-10-25 12:19:49 +0400

    Making codebase neat

---
+++ Testing Application/Interfaces/IBaseEntity.cs
@@ -0,0 +1,12 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace TestingApplication.Interfaces
+{
+    public interface IBaseEntity
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        string? Id { get; set; }
+    }
+}
