80109b9680a479cef35884470a40b0b51017fa1e
Elnur <entreur@gmail.com>
2023-10-25 12:20:11 +0400

    .

---
+++ Testing Application/Model/Base/BaseModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TestingApplication.Model
+{
+    [Obsolete]
+    public class BaseModel
+    {
+        [Key]
+        public int Id { get; set; }
+    }
+}
