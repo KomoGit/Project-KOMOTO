07fc4e5c0668850521aa266b7a609f1c8e6a7549
Elnur <entreur@gmail.com>
2023-10-25 12:19:25 +0400

    .

---
+++ Testing Application/Data Transfer Objects/CompanyCreateDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace TestingApplication.Data_Transfer_Objects
+{
+    public class CompanyCreateDTO
+    {
+        public string CompanyName { get; set; }
+        public string CompanyDescription { get; set; }
+
+        public IFormFile CompanyLogo { get; set; }
+    }
+}
