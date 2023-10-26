9897044af7553307d192fa0e73a4c036839674b7
Elnur <entreur@gmail.com>
2023-10-26 16:07:32 +0400

    Category is now an ENUM

---
+++ Testing Application/Enums/Category.cs
@@ -0,0 +1,12 @@
+﻿namespace TestingApplication.Enums
+{
+    public enum Category
+    {
+        OTHER = 1,
+        SALES = 2,
+        MARKETING = 3,
+        DESIGN = 4,
+        DEVELOPMENT = 5,
+        TRAINING = 6,
+    }
+}
